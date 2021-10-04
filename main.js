7210a110467a8f34b7b046481271bd8e1f100e04
Gerald Buchanan <geraldbuchanan@Geralds-MacBook-Pro.local>
2021-10-03 23:22:13 -0400

    array

--- main.js
+++ main.js
@@ -22,51 +22,113 @@ const students = [
   'Arnold',
   'Andy'
 ];
-
+for (let i=0; i< students.length; i++) {
+  console.log(student[i]);
+}
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
 
 
+
+console.log("for");
+
+for(let i = grades.length - 1; i >=0; i >=0; i = i - 1 {
+  const value = grades[i];
+  console.log('i:', i, 'value:, value');
+})
+
+
 // Challenge 3
 // Console.log out only the even numbers in the following array.
 const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
+for (let i = 0; i <=positiveNumbers.length - 1; i++) {
+  if (positiveNumbers[i] % 2 ===0) {
+    console.log(positiveNumbers[i]);
+  }
+}
 
 
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
+for (let i = 0; i < mixedSignNumbers.length; i++) {
+  if (mixedSignNumbers[i] % 2 === 0 || mixedSignNumbers[i] % 2 ===0 && mixedSignNumbers[i] <=0) {
+    console.log(' The even numbers are at index: ${i} and is the number: ${mixednumbers[i]}');
+  }
+}
 
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
 
+for (let i = 0; i < symmetricalCapitals.length; i++) {
+  console.log(symmetricalCapitals[i]);
+}
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
 const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 
+for (const number of fibonacciNumbers) {
+  console.log(number);
+}
+
 
 // Challenge 7
 // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
 
+const array = [1,2,3,4,5,];
+
+for (let i = 0; i < araay.length; i++) {
+  console.log(value);
+}
 
 // Challenge 8
 // Make a new EMPTY array and fill it with five values of your choice using BOTH .push AND .unshift, and then console.log out each value individually.
 
+console.log('challenge 8!')
 
+const challenged8 = [];
+challenged8.push(1);
+challenged8.push(2);
+challenged8.push(3);
+challenged8.unshift(4);
+challenged8.unshift(5);
+
+for (let i = 0; i < challenged8.length; i++) {
+  const value = challenged8[i];
+  console.log(value);
+}
 // Challenge 9
 // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
-
+for (let i = 3; i <= 10; i++) {
+  console.log(students[i]);
+}
 
 // Challenge 10
 // Loop through the `students` array from Challenge 1, making a COPY of the array, starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
 
+console.log('challenge 10!');
+
+const studentsCopy = []
+
+for (let i = 3; i <=10; i++) {
+  const value = students[i];
+  studentsCopy.push(value);
+}
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
+
+
+console.log('challenge 11!');
+
+const studentsSlice = studetns.slice();
+console.log(studentsSlice);
+console.log(students);
 
 
 // Challenge 12
@@ -74,15 +136,42 @@ const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
 
 
+const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
+
+console.log('challenge 12!')
+
+const splicedValues = dinosaurs.splice(4, 3);{
+console.log(value)
+}
+
 // Challenge 13
 // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
+
+const dinoString = dinosaurs.join('*')
+console.log(dinoString);
 
 
 // Challenge 14
 // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
+
+dinosaurs.reverse();
+console.log(dinosaurs);
+
 
 
 // Challenge 15
 // Use .concat to combine the following two arrays into a NEW array you make (.concat RETURNS an array). Then console.log out each value of that new array individually. Now console.log both the primaries and secondaries arrays, noting that .concat does NOT mutate the original arrays.
 const primaries = ['red', 'yellow', 'blue'];
 const secondaries = ['orange', 'green', 'purple'];
+
+
+
+const thrird = primaries.contact(secondaries); 
+  for (let i = 0; i < thrird.length; i++) {
+  const value = third[i];
+console.log(value);
+}
+
+console.log(primaries);
+console.length(secondaries);
+
